e4bcb1bfeda6f0a41fb0433a082f0cebffb71c68
Yerkebulan <108175003+Yerke11@users.noreply.github.com>
2022-08-02 17:41:00 +0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,31 @@
+
+
+//Full-page view:
+
+//https://codepen.io/GeorgePark/full/VXrwOP/
+
+
+const modal = document.querySelector(".modal");
+const previews = document.querySelectorAll("img.gallery-image");
+const original = document.querySelector('.full-img');
+
+
+previews.forEach((preview) => {
+    preview.addEventListener('click', () => {
+
+        modal.classList.add('open');
+        original.classList.add('open');
+        const originalSrc = preview.getAttribute('src');
+        original.src = originalSrc;
+        console.log(originalSrc)
+
+
+    });
+});
+modal.addEventListener('click',(e) => {
+    if(e.target.classList.contains('modal')){
+        modal.classList.remove('open');
+        original.classList.remove('open');
+    }
+});
+
